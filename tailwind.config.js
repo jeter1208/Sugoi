ea2dbc0698779bd39ebc8e9b80a439a975e4516b
Dennis <dlex351279@gmail.com>
2021-08-29 10:57:10 +0800

    修正Procfile、新增tailwindcss purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './app/**/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.{js, jsx}',
+    './app/views/**/*.html.erb',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
