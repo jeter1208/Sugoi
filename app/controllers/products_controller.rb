128a0ec27f5a57a09eca804d9658c2c878250800
wali <larts393@gmail.com>
2021-08-30 00:43:01 +0800

    crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,48 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path,notice: "新增成功"
+    else
+      render :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to products_path,notice: "修改成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy if @product
+      redirect_to products_path,notice: "刪除成功"
+  end
+
+
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity, :describtion, :category, :material, :manufacturing_method, :country, :content)
   end
 end
